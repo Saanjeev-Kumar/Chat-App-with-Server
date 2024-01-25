cb0361ff731a77ecb71171bdc31b6685a22beee4
Saanjeev Kumar K R <73130090+Saanjeev-Kumar@users.noreply.github.com>
2024-01-25 11:53:17 +0530

    Update mongoUtils.js Removed URI

--- Server/db/mongoUtils.js
+++ Server/db/mongoUtils.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://saanjeev:saanjeev@cluster0.iqret.mongodb.net/Chat____APP"
-// mongodb+srv://saanjeev:qwerty123@cluster0.iqret.mongodb.net/challenge
+const url = ""
+
 
 function mongooseConnect(){
     mongoose.connect(url, {
